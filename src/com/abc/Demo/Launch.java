91fb8c5e31a9ef401d11ea1c88db40d4d9219d40
mzrehm <mzaidurrahman@gmail.com>
2020-06-25 16:50:21 +0530

    firstcommit

---
+++ src/com/abc/Demo/Launch.java
@@ -0,0 +1,11 @@
+package com.abc.Demo;
+
+public class Launch 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome");
+		System.out.println("This is a project Demo");		
+	}
+
+}
